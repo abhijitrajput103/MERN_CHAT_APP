451dc46b8a4202bb8f2e5de2443c0872350454bf
abhijitrajput103 <abhijitrajput103@gmail.com>
2025-06-04 18:26:29 +0530

    "Fixed typo in signup function in useAuthStore.js"

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -29,7 +29,7 @@ export const useAuthStore = create((set, get) => ({
     },
 
     // Signup
-    signup: async (data) => {++
+    signup: async (data) => {
         set({ isSigningUp: true });
         try {
             const res = await axiosInstance.post("/auth/signup", data);
